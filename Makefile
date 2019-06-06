4ab7f9c98b57f36d3fef22d6cfb20c7261a53c83
e8f16b524ff6cbedadc3 <e8f16b524ff6cbedadc3@yandex.com>
2019-06-06 10:23:49 +0800

    save fetch data to json file

--- Makefile
+++ Makefile
@@ -5,3 +5,6 @@ default: run
 
 run:
 	python peanut/main.py
+
+dep_install:
+	python -m pip install -U pip -r requirements.txt
--- peanut/main.py
+++ peanut/main.py
@@ -1,17 +1,26 @@
 # -*- coding: utf-8 -*-
 import argparse
+import datetime
+import json
 import logging
-from typing import List, Iterator
+from typing import Iterator, List, Tuple
 
 from requests_html import HTMLSession
 
 logging.basicConfig(level=logging.INFO)
+LOG = logging.getLogger(__name__)
+
+
+def helper_title(title: str) -> Tuple[Tuple[int, int, int], str]:
+    _, location = title.split('地区', 1)[0].split('日', 1)
+    year, _ = _.split('年')
+    month, day = _.split('月')
+    return ((day, month, year), location)
 
 
 class Crawler:
     def __init__(self):
         self.base_url = 'http://www.62422.cn'
-        self.logger = logging.getLogger(__name__)
         self.session = HTMLSession()
 
     def parse(self) -> List[str]:
@@ -25,17 +34,13 @@ class Crawler:
         resp = self.session.get(url)
         resp.html.encoding = 'gbk'
 
-        _, location = resp.html.find('title', first=True).text.split('地区', 1)[0].split('日', 1)
-        year, _ = _.split('年')
-        month, day = _.split('月')
-        self.logger.info(f'[+] {day}/{month}/{year}: {location}')
+        (day, month, year), location = helper_title(resp.html.find('title', first=True).text)
+        LOG.info(f'[+] {day}/{month}/{year}: {location}')
 
         data = resp.html.text.split('点此查看会员收费标准与办理方式\n', 1)[1].split('\n')
         lst = (it
                for it in data
                if it.startswith(location))
-        for it in lst:
-            self.logger.info(f'[+] entry: {it}')
         return lst
 
 
@@ -49,7 +54,15 @@ def parse_args():
 def main(url: str = ''):
     crawler = Crawler()
     lst = crawler.parse()
-    crawler.parse_details(lst[0].attrs['href'])
+    seen = {}
+    date, _ = helper_title(lst[0].text)
+    for it in lst:
+        _, loc = helper_title(it.text)
+        seen[loc] = list(crawler.parse_details(it.attrs['href']))
+    result_filename = '_data/result_{}.json'.format(datetime.datetime.today().date())
+    with open(result_filename, 'w') as fd:
+        json.dump(seen, fd, ensure_ascii=False)
+    LOG.info(f'[+] write to {result_filename}')
 
 
 if __name__ == '__main__':
