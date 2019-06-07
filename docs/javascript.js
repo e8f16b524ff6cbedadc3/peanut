fdc0ca14158b1084c6367b1a0fc56d68c325116c
e8f16b524ff6cbedadc3 <e8f16b524ff6cbedadc3@yandex.com>
2019-06-07 09:38:05 +0800

    fixx fetching bugs & update data

--- docs/javascript.js
+++ docs/javascript.js
@@ -1,37 +1,36 @@
 const node = document.querySelector("#container"); 
 const today = new Date().toISOString().split('T')[0];
 
-let lastUpdate = today;
 fetch ('.last-update')
 .then(resp => resp.text())
-.then(resp => lastUpdate = resp.trim());
-
-fetch(`_data/result_${lastUpdate}.json`)
-  .then(resp => {
-    if (resp.status !== 200) {
-      throw new Error("Not 200 response");
-    } else {
-      return resp.json();
-    }
-  })
-  .then(resp => {
-    const update_info = document.createElement('p');
-    update_info.textContent = `本页面更新于：${lastUpdate}`;
-    node.appendChild(update_info);
-    for (let location in resp) {
-      const newDiv = document.createElement('div');
-      const newSpan = document.createElement('span');
-      newSpan.textContent = location;
-      newDiv.appendChild(newSpan);
-
-      const newUl = document.createElement('ul');
-      for (let entry of resp[location]) {
-        const newLi = document.createElement('li');
-        newLi.textContent = entry;
-        newUl.appendChild(newLi);
+.then(lastUpdate => {
+  fetch(`_data/result_${lastUpdate.trim()}.json`)
+    .then(resp => {
+      if (resp.status !== 200) {
+        throw new Error(resp.statusText);
+      } else {
+        return resp.json();
       }
-      newDiv.appendChild(newUl);
-      node.appendChild(newDiv);
-    }
-  })
-  .catch(err => node.textContent = err);
+    })
+    .then(resp => {
+      const update_info = document.createElement('p');
+      update_info.textContent = `本页面更新于：${lastUpdate}`;
+      node.appendChild(update_info);
+      for (let location in resp) {
+        const newDiv = document.createElement('div');
+        const newSpan = document.createElement('span');
+        newSpan.textContent = location;
+        newDiv.appendChild(newSpan);
+
+        const newUl = document.createElement('ul');
+        for (let entry of resp[location]) {
+          const newLi = document.createElement('li');
+          newLi.textContent = entry;
+          newUl.appendChild(newLi);
+        }
+        newDiv.appendChild(newUl);
+        node.appendChild(newDiv);
+      }
+    })
+    .catch(err => node.textContent = err);
+});
